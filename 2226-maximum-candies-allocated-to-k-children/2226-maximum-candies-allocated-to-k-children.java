16c59b66f7971b7e81e154e3a4e437b49772fb06
Aditya Pandey <128928890+Aditya08pandey@users.noreply.github.com>
2025-03-14 09:10:05 +0530

    Time: 50 ms (9.01%), Space: 63.1 MB (58.6%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,24 +1,31 @@
 class Solution {
+    public boolean canSplit(int[] candies, long k, long mid) {
+        long split = 0;
+        for(int i = 0; i < candies.length; ++i) {
+            split += candies[i]/mid;
+        }   
+        if(split >= k)
+            return true;
+        else
+            return false;
+    }
+    
     public int maximumCandies(int[] candies, long k) {
-        int left = 1, right = 10_000_000;
-        int result = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long childrenCount = 0;
-
-            for (int candy : candies) {
-                childrenCount += candy / mid;
-            }
-
-            if (childrenCount >= k) {
-                result = mid;
-                left = mid + 1;
+        long sum = 0;
+        for(int i = 0; i < candies.length; ++i) {
+            sum += candies[i];
+        }
+        long start = 1, end = sum;
+        long ans = 0;
+        while(start <= end) {
+            long mid = (start + end)/2;
+            if(canSplit(candies, k, mid)) {
+                ans = mid;
+                start = mid + 1;
             } else {
-                right = mid - 1;
+                end = mid-1;
             }
         }
-
-        return result;
+        return (int)ans;
     }
 }
