41d15fbe2732fec394e1caa5adc3bf6b6e08602d
Aditya Pandey <128928890+Aditya08pandey@users.noreply.github.com>
2025-03-24 08:40:12 +0530

    Time: 36 ms (89.9%), Space: 100.4 MB (55.56%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,24 +1,17 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        int n=meetings.length;
+        Arrays.sort(meetings, (a,b) -> (a[0] - b[0]));
+        int cntDays = meetings[0][0]-1, end = meetings[0][1];
 
-        if(n==1){
-            return days-(meetings[0][1]-meetings[0][0]+1);
+        for(int indx = 1; indx < meetings.length; indx++){
+            int start = meetings[indx][0];
+            if(start > end){
+                cntDays += (start - end - 1);
+            }
+            end = Math.max(end, meetings[indx][1]);
         }
-
-        Arrays.sort(meetings, (a,b)->a[0]-b[0]);
-
-        for(int i=1; i<n; i++){
-            if(meetings[i-1][1]>=meetings[i][0]){
-                meetings[i][0]=meetings[i-1][0];                
-                meetings[i][1]=Math.max(meetings[i-1][1], meetings[i][1]);
-            }else{
-                days-=meetings[i-1][1]-meetings[i-1][0]+1;
-            }   
-        }
-
-        days-=meetings[n-1][1]-meetings[n-1][0]+1;
-
-        return days;
+        if(end < days)
+            return days - end + cntDays;
+        return cntDays;
     }
 }
