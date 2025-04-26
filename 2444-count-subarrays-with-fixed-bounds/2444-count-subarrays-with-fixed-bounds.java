7d891eaff273abd1861f841a014afa6e5f04dd77
Aditya Pandey <128928890+Aditya08pandey@users.noreply.github.com>
2025-04-26 09:23:12 +0530

    Time: 7 ms (95.85%), Space: 60.5 MB (37.69%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,27 +1,27 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        int n = nums.length;
-        long result = 0;
-        int minKIndex = -1;
-        int maxKIndex = -1;
-        int culpritIndex = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < minK || nums[i] > maxK) {
-                culpritIndex = i;
+        long res = 0;
+        boolean minFound = false, maxFound = false;
+        int start = 0, minStart = 0, maxStart = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num < minK || num > maxK) {
+                minFound = false;
+                maxFound = false;
+                start = i+1;
             }
-
-            if (nums[i] == minK)
-                minKIndex = i;
-
-            if (nums[i] == maxK)
-                maxKIndex = i;
-
-            long smaller = Math.min(minKIndex, maxKIndex);
-            long temp = smaller - culpritIndex;
-
-            result += temp <= 0 ? 0 : temp;
+            if (num == minK) {
+                minFound = true;
+                minStart = i;
+            }
+            if (num == maxK) {
+                maxFound = true;
+                maxStart = i;
+            }
+            if (minFound && maxFound) {
+                res += (Math.min(minStart, maxStart) - start + 1);
+            }
         }
-        return result;
+        return res;
     }
 }
